87d3c7e21785c0e4ed0dfcece4f0925988451086
Santelle <sea.write@mail.ru>
2019-12-08 23:24:54 +1000

    Изменение названия корневой папки и названия фрагмента, привязка обработчика нажатий

---
+++ app/src/main/java/com/example/testandrtofriday/ui/bedroom/BedroomFragment.java
@@ -0,0 +1,66 @@
+package com.example.testandrtofriday.ui.bedroom;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.testandrtofriday.R;
+
+public class BedroomFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_bedroom, container, false);
+
+        Button mButtonLight = root.findViewById(R.id.RoomLightButton);
+        Button mButtonRosette = root.findViewById(R.id.RoomRosetteButton);
+        Button mButtonVacuum = root.findViewById(R.id.RoomVacuumButton);
+        Button mButtonCurtains = root.findViewById(R.id.RoomButtonCurtains);
+
+        mButtonLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getContext(), "Вроде включился", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        mButtonRosette.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getContext(), "Вроде включился", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        mButtonVacuum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getContext(), "Вроде включился", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        mButtonCurtains.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getContext(), "Вроде включился", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        return root;
+    }
+}
